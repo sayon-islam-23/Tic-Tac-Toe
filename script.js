51ccfb965a0364b209d6561d24f62ad18c0e6eea
SAYON ISLAM <sayonislam.2000@gmail.com>
2021-10-17 22:24:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const checkWin = ()=>{
 }
 
 // game logic
-music.play()
+music.play();
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
